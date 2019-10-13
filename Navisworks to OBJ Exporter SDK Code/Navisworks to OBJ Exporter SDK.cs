a9ebcbc1d4d54bc85ec608e0a9b4b9aeea758e59
vision <visionworkplace@gmail.com>
2019-10-13 23:18:08 +0530

    Navisworks to OBJ Exporter SDK Code

---
+++ Navisworks to OBJ Exporter SDK Code/Navisworks to OBJ Exporter SDK.cs
@@ -0,0 +1,19 @@
+﻿
+/***********************************************************
+* Step 1: Download the dependent dlls from the mentioned link.
+* Add referance of above dependent dlls in your project using Project->Referancea->Add Referance...
+**********************************************************/
+
+/***********************************************************
+* Step 2: Create the object of 'VisionworkplaceOBJ' class and the call 'ConvertToOBJ' function
+**********************************************************/
+//Setting Apis
+    Visionworkplace_Navisworks_OBJ.Settings.getSettings.Y_Z_Flip = true;
+    Visionworkplace_Navisworks_OBJ.Settings.getSettings.ShowMessageBox = false;
+    Visionworkplace_Navisworks_OBJ.Settings.getSettings.ExportSelecterEntities = false;
+    Visionworkplace_Navisworks_OBJ.Settings.getSettings.Rotate = true;
+    Visionworkplace_Navisworks_OBJ.Settings.getSettings.Properties = false;
+
+//Navisworks to OBJ Export Apis.
+    Visionworkplace_Navisworks_OBJ.VisionworkplaceOBJ objConverter = new Visionworkplace_Navisworks_OBJ.VisionworkplaceOBJ();
+    objConverter.ConvertToOBJ("E:\\Test\\Navisworks_OBJ.obj");
